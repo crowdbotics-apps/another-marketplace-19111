b9597d29e86f07e3faffa73254b888fb8257678f
Crowdbotics <team@crowdbotics.com>
2020-08-04 21:14:28 +0000

    Update components

--- src/features/BlankScreen183007/screens/blank.js
+++ src/features/BlankScreen183007/screens/blank.js
@@ -727,7 +727,41 @@ export class _Blank extends React.Component {
                       borderRadius: 0,
                       backgroundSize: "auto"
                     }}
-                  />
+                  >
+                    <Text
+                      style={{
+                        marginLeft: 0,
+                        marginRight: 0,
+                        marginTop: 5,
+                        marginBottom: 5,
+                        paddingLeft: 5,
+                        paddingRight: 5,
+                        paddingTop: 5,
+                        paddingBottom: 5,
+                        overflow: "visible",
+                        textAlign: "left",
+                        verticalAlign: "baseline",
+                        fontSize: 12,
+                        color: "#000000",
+                        backgroundColor: "#ffffff",
+                        fontFamily: "Roboto-Regular",
+                        borderColor: "#000000",
+                        borderStyle: "solid",
+                        borderWidth: 0,
+                        borderLeftWidth: 0,
+                        borderRightWidth: 0,
+                        borderTopWidth: 0,
+                        borderBottomWidth: 0,
+                        borderRadius: 0,
+                        textDecorationLine: "none",
+                        textTransform: "none",
+                        lineHeight: 12,
+                        letterSpacing: 0
+                      }}
+                    >
+                      2 hours ago
+                    </Text>
+                  </View>
                 </View>
               </View>
               <View
@@ -1111,7 +1145,41 @@ export class _Blank extends React.Component {
                       borderRadius: 0,
                       backgroundSize: "auto"
                     }}
-                  />
+                  >
+                    <Text
+                      style={{
+                        marginLeft: 0,
+                        marginRight: 0,
+                        marginTop: 5,
+                        marginBottom: 5,
+                        paddingLeft: 5,
+                        paddingRight: 5,
+                        paddingTop: 5,
+                        paddingBottom: 5,
+                        overflow: "visible",
+                        textAlign: "left",
+                        verticalAlign: "baseline",
+                        fontSize: 12,
+                        color: "#000000",
+                        backgroundColor: "#ffffff",
+                        fontFamily: "Roboto-Regular",
+                        borderColor: "#000000",
+                        borderStyle: "solid",
+                        borderWidth: 0,
+                        borderLeftWidth: 0,
+                        borderRightWidth: 0,
+                        borderTopWidth: 0,
+                        borderBottomWidth: 0,
+                        borderRadius: 0,
+                        textDecorationLine: "none",
+                        textTransform: "none",
+                        lineHeight: 12,
+                        letterSpacing: 0
+                      }}
+                    >
+                      Yesterday
+                    </Text>
+                  </View>
                 </View>
               </View>
               <View
@@ -1495,7 +1563,41 @@ export class _Blank extends React.Component {
                       borderRadius: 0,
                       backgroundSize: "auto"
                     }}
-                  />
+                  >
+                    <Text
+                      style={{
+                        marginLeft: 0,
+                        marginRight: 0,
+                        marginTop: 5,
+                        marginBottom: 5,
+                        paddingLeft: 5,
+                        paddingRight: 5,
+                        paddingTop: 5,
+                        paddingBottom: 5,
+                        overflow: "visible",
+                        textAlign: "left",
+                        verticalAlign: "baseline",
+                        fontSize: 12,
+                        color: "#000000",
+                        backgroundColor: "#ffffff",
+                        fontFamily: "Roboto-Regular",
+                        borderColor: "#000000",
+                        borderStyle: "solid",
+                        borderWidth: 0,
+                        borderLeftWidth: 0,
+                        borderRightWidth: 0,
+                        borderTopWidth: 0,
+                        borderBottomWidth: 0,
+                        borderRadius: 0,
+                        textDecorationLine: "none",
+                        textTransform: "none",
+                        lineHeight: 12,
+                        letterSpacing: 0
+                      }}
+                    >
+                      Friday
+                    </Text>
+                  </View>
                 </View>
               </View>
               <View
