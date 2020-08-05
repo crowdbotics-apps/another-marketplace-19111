90a9461d6c5390b3974b754ab3a1f0a4c435c468
Crowdbotics <team@crowdbotics.com>
2020-08-05 23:05:47 +0000

    Update components

--- src/features/BlankScreen288031/screens/blank.js
+++ src/features/BlankScreen288031/screens/blank.js
@@ -199,7 +199,38 @@ export class _Blank extends React.Component {
                 borderRadius: 0,
                 backgroundSize: "auto"
               }}
-            />
+            >
+              <Image
+                resizeMode="contain"
+                source={{
+                  uri:
+                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/lady.png"
+                }}
+                style={{
+                  width: "100%",
+                  height: 50,
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginTop: 0,
+                  marginBottom: 0,
+                  paddingLeft: 0,
+                  paddingRight: 0,
+                  paddingTop: 0,
+                  paddingBottom: 0,
+                  overflow: "visible",
+                  textAlign: "left",
+                  verticalAlign: "baseline",
+                  borderColor: "#000000",
+                  borderStyle: "solid",
+                  borderWidth: 0,
+                  borderLeftWidth: 0,
+                  borderRightWidth: 0,
+                  borderTopWidth: 0,
+                  borderBottomWidth: 0,
+                  borderRadius: 0
+                }}
+              />
+            </View>
             <View
               style={{
                 width: "70%",
@@ -234,7 +265,41 @@ export class _Blank extends React.Component {
                 borderRadius: 0,
                 backgroundSize: "auto"
               }}
-            />
+            >
+              <Text
+                style={{
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginTop: 5,
+                  marginBottom: 5,
+                  paddingLeft: 5,
+                  paddingRight: 5,
+                  paddingTop: 5,
+                  paddingBottom: 5,
+                  overflow: "visible",
+                  textAlign: "left",
+                  verticalAlign: "baseline",
+                  fontSize: 12,
+                  color: "#716a6a",
+                  backgroundColor: "#ffffff",
+                  fontFamily: "Roboto-Regular",
+                  borderColor: "#000000",
+                  borderStyle: "solid",
+                  borderWidth: 0,
+                  borderLeftWidth: 0,
+                  borderRightWidth: 0,
+                  borderTopWidth: 0,
+                  borderBottomWidth: 0,
+                  borderRadius: 0,
+                  textDecorationLine: "none",
+                  textTransform: "none",
+                  lineHeight: 12,
+                  letterSpacing: 0
+                }}
+              >
+                Search
+              </Text>
+            </View>
           </View>
         </View>
         <View
@@ -271,7 +336,41 @@ export class _Blank extends React.Component {
             borderRadius: 0,
             backgroundSize: "auto"
           }}
-        />
+        >
+          <Text
+            style={{
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 5,
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              fontSize: 15,
+              color: "#fd6f80",
+              backgroundColor: "#f4f5f9",
+              fontFamily: "Roboto-Regular",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0,
+              textDecorationLine: "none",
+              textTransform: "none",
+              lineHeight: 12,
+              letterSpacing: 0
+            }}
+          >
+            Saved Items
+          </Text>
+        </View>
         <View
           style={{
             width: "100%",
